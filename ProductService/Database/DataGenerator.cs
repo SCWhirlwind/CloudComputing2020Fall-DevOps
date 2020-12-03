bd7c6bb31b41d9b7b8225faaaac895e8bb1bb90f
Andrew <heyitstranman@yahoo.com>
2020-12-02 18:04:52 -0800

    changes

--- ProductService/Database/DataGenerator.cs
+++ ProductService/Database/DataGenerator.cs
@@ -20,14 +20,14 @@ public class DataGenerator
                 new Product
                 {
                     ProductId = 1,
-                    Name = "Television",
+                    Name = "Monitor",
                     Price = 199.95m,
                     Count = 4
                 },
                 new Product
                 {
                     ProductId = 2,
-                    Name = "Sofa",
+                    Name = "Table",
                     Price = 299.95m,
                     Count = 10
                 }
